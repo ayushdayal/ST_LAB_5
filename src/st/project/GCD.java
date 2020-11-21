0ea726b0f8172d41a073da22474ff6a5bf51bb8d
ayush <ayushdayal1998@gmail.com>
2020-11-21 22:23:12 +0530

    Initial commit

---
+++ src/st/project/GCD.java
@@ -0,0 +1,16 @@
+package st.project;
+
+public class GCD {
+    public static int findGCD(int n1, int n2) throws Exception {
+        int gcd = 1;
+        if (n1 < 1 || n1 > 100 || n2 < 1 || n2 > 100) {
+            throw new Exception("asdf");
+        }
+        for (int i = 1; i <= n1 && i <= n2; ++i) {
+            // Checks if i is factor of both integers
+            if (n1 % i == 0 && n2 % i == 0)
+                gcd = i;
+        }
+        return gcd;
+    }
+}
---
+++ src/st/project/Main.java
@@ -0,0 +1,12 @@
+package st.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String str = "https://www.youtube.com/watch?v=-lmoNFqLT64";
+        String[] strp = str.split("/");
+
+    }
+}
